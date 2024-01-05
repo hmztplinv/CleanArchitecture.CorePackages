98c713982156126971f467bcdb34c1a01e337e59
hmztplinv <hmz.tpal@gmail.com>
2024-01-05 12:13:41 +0300

    dynamic

--- CleanArchitecture.CorePackages/CleanArchitecture.CorePackages.Persistance/Dynamic/IQueryableDynamicFilterExtensions.cs
+++ CleanArchitecture.CorePackages/CleanArchitecture.CorePackages.Persistance/Dynamic/IQueryableDynamicFilterExtensions.cs
@@ -6,112 +6,111 @@ using System.Windows.Markup;
 
 public static class IQueryableDynamicFilterExtensions
 {
-    private static readonly string[] _orders = { "asc", "desc" }; // sıralama yönleri
-    private static readonly string[] _logics = { "and", "or" }; // mantıksal operatörler
-    
-    private static readonly IDictionary<string,string> _operators=new Dictionary<string, string>
+    private static readonly string[] _orders = { "asc", "desc" };
+    private static readonly string[] _logics = { "and", "or" };
+
+    private static readonly IDictionary<string, string> _operators = new Dictionary<string, string>
     {
-        {"eq","=="}, // equal
-        {"neq","!="}, // not equal
-        {"lt","<"}, // less than
-        {"lte","<="}, // less than or equal
-        {"gt",">"}, // greater than
-        {"gte",">="}, // greater than or equal
-        {"contains","Contains"}, // contains
-        {"doesnotcontain","DoesNotContain"}, // does not contain
-        {"startswith","StartsWith"}, // starts with
-        {"endswith","EndsWith"}, // ends with
-        {"isnull","=="}, // is null
-        {"isnotnull","!="}, // is not null
-        {"isempty","=="}, // is empty
-        {"isnotempty","!="} // is not empty
+        { "eq", "=" },
+        { "neq", "!=" },
+        { "lt", "<" },
+        { "lte", "<=" },
+        { "gt", ">" },
+        { "gte", ">=" },
+        { "isnull", "== null" },
+        { "isnotnull", "!= null" },
+        { "startswith", "StartsWith" },
+        { "endswith", "EndsWith" },
+        { "contains", "Contains" },
+        { "doesnotcontain", "Contains" }
     };
 
-    public static IQueryable<T> ToDynamic<T>(this IQueryable<T> query, DynamicQuery dynamicQuery) // IQueryable<T> yi DynamicQuery'e çevirir
+    public static IQueryable<T> ToDynamic<T>(this IQueryable<T> query, DynamicQuery dynamicQuery)
     {
         if (dynamicQuery.Filter is not null)
             query = Filter(query, dynamicQuery.Filter);
-
         if (dynamicQuery.Sort is not null && dynamicQuery.Sort.Any())
             query = Sort(query, dynamicQuery.Sort);
         return query;
     }
 
-    private static IQueryable<T> Filter<T>(IQueryable<T> queryable,Filter filter) // filtrelerin uygulanması
+    private static IQueryable<T> Filter<T>(IQueryable<T> queryable, Filter filter)
     {
-        IList<Filter> filters=GetAllFilters(filter);
-        string?[] values=filters.Select(x => x.Value).ToArray();
-        string where =Transform(filter,filters);
-        if(!string.IsNullOrEmpty(where) && values != null)
-            queryable=queryable.Where(where,values);
+        IList<Filter> filters = GetAllFilters(filter);
+        string?[] values = filters.Select(f => f.Value).ToArray();
+        string where = Transform(filter, filters);
+        if (!string.IsNullOrEmpty(where) && values != null)
+            queryable = queryable.Where(where, values);
+
         return queryable;
     }
 
-    private static IQueryable<T> Sort<T>(IQueryable<T> queryable,IEnumerable<Sort> sorts) // sıralamanın uygulanması
+    private static IQueryable<T> Sort<T>(IQueryable<T> queryable, IEnumerable<Sort> sort)
     {
-        foreach (Sort sort in sorts)
+        foreach (Sort item in sort)
         {
-            if(string.IsNullOrEmpty(sort.Field))
-                throw new ArgumentException("Field is invalid");
-            if(string.IsNullOrEmpty(sort.Dir) || !_orders.Contains(sort.Dir.ToLower()))
-                throw new ArgumentException("Dir is invalid");
+            if (string.IsNullOrEmpty(item.Field))
+                throw new ArgumentException("Invalid Field");
+            if (string.IsNullOrEmpty(item.Dir) || !_orders.Contains(item.Dir))
+                throw new ArgumentException("Invalid Order Type");
         }
 
-        if(sorts.Any())
+        if (sort.Any())
         {
-            string orderBy =string.Join(separator: "," , values: sorts.Select(x => $"{x.Field} {x.Dir}"));
-            queryable=queryable.OrderBy(orderBy);
+            string ordering = string.Join(separator: ",", values: sort.Select(s => $"{s.Field} {s.Dir}"));
+            return queryable.OrderBy(ordering);
         }
+
         return queryable;
     }
 
     public static IList<Filter> GetAllFilters(Filter filter)
     {
-        IList<Filter> filters=new List<Filter>(); // filtrelerin hepsini alır
-        GetFilters(filter,filters); 
+        List<Filter> filters = new();
+        GetFilters(filter, filters);
         return filters;
     }
 
-    private static void GetFilters(Filter filter, IList<Filter> filters) // filtreleri alır
+    private static void GetFilters(Filter filter, IList<Filter> filters)
     {
-        filters.Add(filter); // filtrelerin varsa alt filtrelerini de alır
+        filters.Add(filter);
         if (filter.Filters is not null && filter.Filters.Any())
-            foreach (Filter f in filter.Filters)
-                GetFilters(f, filters);
+            foreach (Filter item in filter.Filters)
+                GetFilters(item, filters);
     }
 
-    public static string Transform(Filter filter, IList<Filter> filters) // filtreleri dönüştürür
+    public static string Transform(Filter filter, IList<Filter> filters)
     {
         if (string.IsNullOrEmpty(filter.Field))
-            throw new ArgumentException("Field is invalid");
-        if (string.IsNullOrEmpty(filter.Operator) || !_operators.ContainsKey(filter.Operator.ToLower()))
-            throw new ArgumentException("Operator is invalid");
+            throw new ArgumentException("Invalid Field");
+        if (string.IsNullOrEmpty(filter.Operator) || !_operators.ContainsKey(filter.Operator))
+            throw new ArgumentException("Invalid Operator");
 
-        int index = filters.IndexOf(filter); // filtrelerin indexini alır
-        string comparison = _operators[filter.Operator.ToLower()]; // filtrelerin operatörlerini alır
-        StringBuilder where = new(); // filtreleri oluşturur
+        int index = filters.IndexOf(filter);
+        string comparison = _operators[filter.Operator];
+        StringBuilder where = new();
+
         if (!string.IsNullOrEmpty(filter.Value))
         {
-            if (filter.Operator.ToLower() == "contains" || filter.Operator.ToLower() == "doesnotcontain") // where ile StringBuilder
-                where.Append($"!np({filter.Field}).{comparison}(@{index.ToString()})"); // np: null propagation, null verileri de ver ya da verme
-            else if (comparison == "StartsWith" || comparison == "EndsWith" || comparison == "Contains")
-                where.Append($"np({filter.Field}).{comparison}(@{index.ToString()})");
+            if (filter.Operator == "doesnotcontain")
+                where.Append($"(!np({filter.Field}).{comparison}(@{index.ToString()}))");
+            else if (comparison is "StartsWith" or "EndsWith" or "Contains")
+                where.Append($"(np({filter.Field}).{comparison}(@{index.ToString()}))");
             else
-                where.Append($"np({filter.Field}).{comparison}(@{index.ToString()})");
+                where.Append($"np({filter.Field}) {comparison} @{index.ToString()}");
         }
-        else if (filter.Operator is "isnull" || filter.Operator is "isnotnull")
-            where.Append($"np({filter.Field}).{comparison}");
-        else if (filter.Operator is "isempty" || filter.Operator is "isnotempty")
-            where.Append($"np({filter.Field}).{comparison}");
-        
+        else if (filter.Operator is "isnull" or "isnotnull")
+        {
+            where.Append($"np({filter.Field}) {comparison}");
+        }
+
         if (filter.Logic is not null && filter.Filters is not null && filter.Filters.Any())
         {
-            if (!_logics.Contains(filter.Logic.ToLower()))
-                throw new ArgumentException("Logic is invalid");
-            return $"({where.ToString()} {filter.Logic.ToLower()} ({string.Join(separator: $" {filter.Logic.ToLower()} ", values: filter.Filters.Select(x => Transform(x, filters)))}))";
+            if (!_logics.Contains(filter.Logic))
+                throw new ArgumentException("Invalid Logic");
+            return $"{where} {filter.Logic} ({string.Join(separator: $" {filter.Logic} ", value: filter.Filters.Select(f => Transform(f, filters)).ToArray())})";
         }
-        
+
         return where.ToString();
     }
-
 }
